bf41d32c97898ce795aca1aca62f172e7d829282
Abbey Dent <adent@g.clemson.edu>
2019-05-21 13:08:42 -0400

    Seperate dataset into target and data

--- run_knn.py
+++ run_knn.py
@@ -4,3 +4,13 @@ from sklearn.neighbors import KNeighborsClassifier
 iris = pd.read_csv("iris.csv", header=None)
 
 print(iris.head())
+
+#data = iris[iris.columns[2:6]]
+
+data = iris.iloc[:,2:6]
+
+print(data.head())
+
+target = iris.iloc[:,1]
+
+print(target)
